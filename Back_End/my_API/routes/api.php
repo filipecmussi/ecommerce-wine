36e950cfcf9eddb50e42499740ff51bdc254fece
@justinojoao <joaojoaojust@gmail.com>
2023-02-09 08:56:42 -0300

    subindo

--- Back_End/my_API/routes/api.php
+++ Back_End/my_API/routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/testes', function () {
+    return "Haved successfull";
+});
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
