8674ce4c281eba1e9cc73c093554a6e21a5d4eb5
@justinojoao <joaojoaojust@gmail.com>
2023-02-09 09:24:40 -0300

    Subindo as migrates

---
+++ Back_End/my_API/database/migrations/2023_02_09_120310_create_vinhos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vinhos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome')->unique(); //o unique tem a função de não deixar o nome se repetir
+            $table->string('descricao');
+            $table->string('valor');
+            $table->timestamps(); //data de criação e update.
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vinhos');
+    }
+};
